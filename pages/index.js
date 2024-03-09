be2835ee55c1fd86e668d7297a8715321b823d5c
camila1603-santos <162054885+camila1603-santos@users.noreply.github.com>
2024-03-09 15:28:00 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Oie Alissu! Estou aprendendo a programar, viu :D</h1>
+    return <h1> Oie Alissu! Estou aprendendo a programar, viu :D hello world haha </h1>
 }
 
 export default Home; 
